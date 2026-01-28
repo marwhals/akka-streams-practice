e3590efb660f7bb8e851288c383cfc8dea3d2a5c
marwhals <marjan.mubarok2012@gmail.com>
2026-01-28 05:10:48 +0000

    Advanced Backpressure

---
+++ src/main/scala/part4_techniques/AdvancedBackpress.scala
@@ -0,0 +1,77 @@
+package part4_techniques
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+
+import java.util.Date
+import scala.language.postfixOps
+
+/**
+ * Another technique to detach the rate of consumption of elements from the rate of production of elements
+ * Use in the case of a slow producer and a fast consumer
+ */
+
+object AdvancedBackpressure extends App {
+
+  implicit val system = ActorSystem("AdvancedBackpressure")
+  implicit val materializer = ActorMaterializer()
+
+  // controlled backpressure
+  val controlledFlow = Flow[Int].map(_ * 2).buffer(10, OverflowStrategy.dropHead)
+
+  case class PagerEvent(description: String, date: Date, nInstances: Int = 1)
+  case class Notification(email: String, pagerEvent: PagerEvent)
+
+  val events = List(
+    PagerEvent("Service discovery failed", new Date),
+    PagerEvent("Illegal elements in the data pipeline", new Date),
+    PagerEvent("Number of HTTP 500 spiked", new Date),
+    PagerEvent("A service stopped responding", new Date)
+  )
+
+  val eventSource = Source(events)
+  val oncallEngineer = "daniel@rockthejvm.com" // a fast service for fetching oncall emails
+
+  def sendEmail(notification: Notification) =
+    println(s"Dear ${notification.email}, you have an event: ${notification.pagerEvent}") // actually send an email
+
+  val notificationSink = Flow[PagerEvent].map(event => Notification(oncallEngineer, event))
+    .to(Sink.foreach[Notification](sendEmail))
+
+  eventSource.to(notificationSink).run()
+
+  /**
+   * Un-backpressureable source
+   */
+
+
+  def sendEmailSlow(notification: Notification) = {
+    Thread.sleep(1000)
+    println(s"Dear ${notification.email}, you have an event: ${notification.pagerEvent}") // actually send an email
+  }
+
+  val aggregateNotificationFlow = Flow[PagerEvent]
+    .conflate((event1, event2) => {
+      val nInstances = event1.nInstances + event2.nInstances
+      PagerEvent(s"You have $nInstances events that require your attention", new Date, nInstances)
+    })
+    .map(resultingEvent => Notification(oncallEngineer, resultingEvent))
+
+  eventSource.via(aggregateNotificationFlow).async.to(Sink.foreach[Notification](sendEmailSlow)).run()
+  // ^^ this is an alternative to backpressure
+
+  /*
+    Slow producers: extrapolate / expand
+   */
+  import scala.concurrent.duration._
+  val slowCounter = Source(Stream.from(1)).throttle(1, 1 second)
+  val hungrySink = Sink.foreach[Int](println)
+
+  val extrapolator = Flow[Int].extrapolate(element => Iterator.from(element))
+  val repeater = Flow[Int].extrapolate(element => Iterator.continually(element))
+
+  slowCounter.via(repeater).to(hungrySink).run()
+
+  val expander = Flow[Int].expand(element => Iterator.from(element))
+}
